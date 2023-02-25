c54a0f11beb8768ae5ea03ff7a3b68bf049e13f6
ohsblazerbots <ohsblazerbots@gmail.com>
2023-02-25 10:48:10 -0700

    New command for autoDrive

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,11 +5,15 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
@@ -20,8 +24,8 @@ public final class Constants {
     public static final int kRightMotor2Port = 18;
   }
 
-  public static final class ElevatorConstants { 
-    public static final int kElevMotorPort = 25; // 25 is a placeholder
+  public static final class ElevatorConstants {
+    public static final int kElevMotorPort = 3; // 25 is a placeholder
   }
 
   public static final class ArmConstants {
@@ -29,12 +33,15 @@ public final class Constants {
   }
 
   public static final class ClawConstants {
-    public static final int kClawMotorPort = 19; // 19 is a placeholder
+    public static final int kClawMotorPort = 8; // 19 is a placeholder
   }
 
-  public static final class LightingConstants {  }
+  public static final class LightingConstants {
+  }
 
-  public static final class AutoConstants {  }
+  public static final class AutoConstants {
+    public static final double kDriveTimeSeconds = 2;
+  }
 
   public static final class IOConstants {
     public static final int kDriverControllerPort = 0;
