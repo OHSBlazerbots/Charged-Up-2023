ed7bb09eb6759dda4737cf342d506b3491392b2d
ohsblazerbots <ohsblazerbots@gmail.com>
2023-02-25 10:48:58 -0700

    New format for Subsystems

--- src/main/java/frc/robot/subsystems/ClawSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClawSubsystem.java
@@ -4,19 +4,18 @@ import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import frc.robot.Constants.ClawConstants;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ClawSubsystem extends SubsystemBase {
     private static final WPI_TalonSRX m_clawMotor = new WPI_TalonSRX(ClawConstants.kClawMotorPort);
 
-    public ClawSubsystem(){
+    public ClawSubsystem() {
         m_clawMotor.configFactoryDefault();
 
         m_clawMotor.setNeutralMode(NeutralMode.Brake);
     }
 
-    public void setClawSpeed(double speed){
-        m_clawMotor.set(speed);    
+    public void setClawSpeed(double speed) {
+        m_clawMotor.set(speed);
     }
 }
--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -4,20 +4,19 @@ import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import frc.robot.Constants.ElevatorConstants;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ElevatorSubsystem extends SubsystemBase{
-     private static final WPI_TalonSRX m_elevMotor = new WPI_TalonSRX(ElevatorConstants.kElevMotorPort);
+public class ElevatorSubsystem extends SubsystemBase {
+   private static final WPI_TalonSRX m_elevMotor = new WPI_TalonSRX(ElevatorConstants.kElevMotorPort);
 
-     public ElevatorSubsystem(){
-        m_elevMotor.configFactoryDefault();
+   public ElevatorSubsystem() {
+      m_elevMotor.configFactoryDefault();
 
-        m_elevMotor.setNeutralMode(NeutralMode.Brake);
-     }
+      m_elevMotor.setNeutralMode(NeutralMode.Brake);
+   }
 
-     public void setElevatorSpeed(double speed){
-        m_elevMotor.set(speed);
-     }
+   public void setElevatorSpeed(double speed) {
+      m_elevMotor.set(speed);
+   }
 
 }
