c2464670d715e8940c93bf0b4813de2bc82183a3
ohsblazerbots <ohsblazerbots@gmail.com>
2023-03-22 12:55:20 -0600

    Change Arm subsystem to wench subsystem and make use new motor controller

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -1,15 +1,14 @@
 package frc.robot.subsystems;
 
 import frc.robot.Constants.ElevatorConstants;
-import frc.robot.subsystems.motor_controllers.PositionPidDualMotorController;
+import frc.robot.subsystems.motor_controllers.PositionPidMotorController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ElevatorSubsystem extends SubsystemBase {
-   private static final PositionPidDualMotorController m_elevController = new PositionPidDualMotorController(
+   private static final PositionPidMotorController m_elevController = new PositionPidMotorController(
          ElevatorConstants.kElevGains,
-         ElevatorConstants.kElevMotorPrimaryPort,
-         ElevatorConstants.kElevMotorSecondaryPort);
+         ElevatorConstants.kElevMotorPort);
 
    public ElevatorSubsystem() {
       m_elevController.zeroSensors();
--- src/main/java/frc/robot/subsystems/WenchSubsystem.java
+++ src/main/java/frc/robot/subsystems/WenchSubsystem.java
@@ -1,21 +1,54 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-
 import frc.robot.Constants.WenchConstants;
+import frc.robot.subsystems.motor_controllers.PositionPidDualMotorController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class WenchSubsystem extends SubsystemBase {
-    private static final WPI_TalonSRX m_wenchMotor = new WPI_TalonSRX(WenchConstants.kWenchPrimaryMotorPort);
+    private static final PositionPidDualMotorController m_wenchController = new PositionPidDualMotorController(
+            WenchConstants.kWenchGains,
+            WenchConstants.kWenchPrimaryMotorPort,
+            WenchConstants.kWenchSecondaryMotorPort);
 
     public WenchSubsystem() {
-        m_wenchMotor.configFactoryDefault();
-
-        m_wenchMotor.setNeutralMode(NeutralMode.Brake); // ".Brake" is placehoder, not sure if we need brake or coast.
+        // TODO: determine if both calls are needed
+        zeroSensors();
+        setPositionZero();
     }
 
-    public void setArmSpeed(double speed) {
-        m_wenchMotor.set(speed);
+    public void zeroSensors() {
+        m_wenchController.zeroSensors();
     }
+
+    public void setPositionZero() {
+        m_wenchController.setPositionZero();
+    }
+
+    public void setWenchPosition(double targetPosition) {
+        double motorPosition = targetPosition;
+        m_wenchController.goToPosition(motorPosition);
+        writeMetricsToSmartDashboard();
+    }
+
+    /**
+     * For manual control of motor
+     * 
+     * @param speed
+     */
+    public void setWenchSpeed(double speed) {
+        m_wenchController.setOutput(speed);
+        writeMetricsToSmartDashboard();
+    }
+
+    public void writeMetricsToSmartDashboard() {
+        SmartDashboard.putNumber("Wench Relative Position", m_wenchController.getCurrentRelativePosition());
+        SmartDashboard.putNumber("Wench Absolute Position", m_wenchController.getCurrentAbsolutePosition());
+        SmartDashboard.putNumber("Wench Goal", m_wenchController.getTargetPosition());
+        SmartDashboard.putNumber("Wench Motor set output", m_wenchController.getOutput());
+    }
+
+    // public void goToMaxOpenPosition() {
+    // setWenchPosition(WenchConstants.kWenchEncoderRotationsAtMaxExtension);
+    // }
 }
